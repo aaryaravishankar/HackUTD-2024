2b9513d5bd32a2ec0b744386041699396d434427
aaryaravishankar <aaryaravishankar@gmail.com>
2024-11-16 15:10:07 -0600

    combined datasets

--- data.py
+++ data.py
@@ -21,36 +21,31 @@ dfRuthless.to_csv(r'HackUTD-2024\data\Ruthless_745H-10_01-10_08.csv', index=Fals
 print(dfRuthless)
 
 
+# Check current working directory
+print("Current Working Directory:", os.getcwd())
+
+# List of CSV filenames in the 'data' folder
+csv_files = [
+    r'HackUTD-2024\data\Bold_744H-10_31-11_07.csv',
+    r'HackUTD-2024\data\Courageous_729H-09_25-09_28.csv',
+    r'HackUTD-2024\data\Fearless_709H-10_31-11_07.csv',
+    r'HackUTD-2024\data\Gallant_102H-10_04-10_11.csv',
+    r'HackUTD-2024\data\Noble_4H-10_24-10_29.csv',
+    r'HackUTD-2024\data\Resolute_728H-10_14-10_21.csv',
+    r'HackUTD-2024\data\Ruthless_745H-10_01-10_08.csv',
+    r'HackUTD-2024\data\Steadfast_505H-10_30-11_07.csv',
+    r'HackUTD-2024\data\Valiant_505H-09_22-09_30.csv'
+]
 
 
+# Read each CSV file into a dataframe
+dataframes = [pd.read_csv(file) for file in csv_files]
 
+# Combine all DataFrames into one
+combined_df = pd.concat(dataframes, ignore_index=True)
 
+# Display the combined DataFrame
+print(combined_df)
 
-# # Check current working directory
-# print("Current Working Directory:", os.getcwd())
-
-# # List of CSV filenames in the 'data' folder
-# csv_files = [
-#     r'HackUTD-2024\data\Bold_744H-10_31-11_07.csv',
-#     r'HackUTD-2024\data\Courageous_729H-09_25-09_28.csv',
-#     r'HackUTD-2024\data\Fearless_709H-10_31-11_07.csv',
-#     r'HackUTD-2024\data\Gallant_102H-10_04-10_11.csv',
-#     r'HackUTD-2024\data\Noble_4H-10_24-10_29.csv',
-#     r'HackUTD-2024\data\Resolute_728H-10_14-10_21.csv',
-#     r'HackUTD-2024\data\Ruthless_745H-10_01-10_08.csv',
-#     r'HackUTD-2024\data\Steadfast_505H-10_30-11_07.csv',
-#     r'HackUTD-2024\data\Valiant_505H-09_22-09_30.csv'
-# ]
-
-
-# # Read each CSV file into a dataframe
-# dataframes = [pd.read_csv(file) for file in csv_files]
-
-# # Combine all DataFrames into one
-# combined_df = pd.concat(dataframes, ignore_index=True)
-
-# # Display the combined DataFrame
-# print(combined_df)
-
-# # making a new csv file for the data 
-
+# making a new csv file for the data 
+combined_df.to_csv(r'combinedDataset.csv', index=False)
